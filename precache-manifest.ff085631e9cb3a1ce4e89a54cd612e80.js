4a30eb7495bc98a9c8f37095b66138c21367fb5e
dlxx <dlxxdlcc@gmail.com>
2022-09-17 19:47:07 +0800

    deploy

---
+++ precache-manifest.ff085631e9cb3a1ce4e89a54cd612e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3489b30a31b1fd5da07",
+    "url": "/paramecium-website/css/app.1b7c2b8c.css"
+  },
+  {
+    "revision": "3e131e5570975a2ff1a0ce3389547d04",
+    "url": "/paramecium-website/index.html"
+  },
+  {
+    "revision": "a3489b30a31b1fd5da07",
+    "url": "/paramecium-website/js/app.a9b0e624.js"
+  },
+  {
+    "revision": "8d22f1b82194535b0516",
+    "url": "/paramecium-website/js/chunk-vendors.0f22d680.js"
+  },
+  {
+    "revision": "5e7c2f38a0d3e4049d5da3bbba13876a",
+    "url": "/paramecium-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/paramecium-website/robots.txt"
+  }
+]);
